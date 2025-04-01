81958a33b9ea9b1c88956e6983792590c4d6d202
Eliška Štěpánová <stepanova.eliska01@gmail.com>
2025-04-01 23:22:51 +0200

    Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const questionElement = document.querySelector('.question');
+questionElement.textContent ='Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.';
 
+const userAnswer = prompt('Vaše odpověď');
+const answerTextElement = document.querySelector('.answer__text');
+answerTextElement.textContent = userAnswer
+
+const answerElement = document.querySelector('.answer')
+if (userAnswer === 'Bell') {
+    answerElement.classList.add('answer--correct');
+} else {
+    answerElement.classList.add('answer--wrong');
+
+}
